667bcaaaebf5847f582c8012583269d452664115
duy-lvl <duylvlse160831@fpt.edu.vn>
2023-02-23 09:34:22 +0700

    first commit

---
+++ PRN221/PRN221/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRN221.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
